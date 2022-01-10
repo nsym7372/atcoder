9e913cd164fbc2ef3bddf3542b391293791c467c
nsym7372 <nsym7372@outlook.jp>
2022-01-10 15:41:00 +0900

    ac

--- ABS/ABC085C.py
+++ ABS/ABC085C.py
@@ -2,9 +2,13 @@ import sys
 n, y = map(int, input().split())
 
 for i in range(0, n + 1):
-    for j in range(i, n + 1):
-        for k in range(j, n + 1):
-            if i * 10000 + j * 5000 + k * 1000 == y:
-                print(f"{i} {j} {k}")
-                sys.exit()
+    for j in range(0, n + 1):
+        k = n - i - j
+        # print(f"i: {i} / j: {j} / k: {k}")
+        if k < 0 or i * 10000 + j * 5000 + k * 1000 != y:
+            continue
+        else:
+            print(f"{i} {j} {k}")
+            sys.exit()
+
 print(-1, -1, -1)
